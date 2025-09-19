e6945a2e5cbe85e0585ee5172dc14d626f169266
matiasluq-ux <matiasluq@gmail.com>
2025-09-19 00:20:49 -0300

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar({ onToggleAdmin, onNavigate, currentView, onToggl
   };
 
   return (
-    <nav className="bg-white shadow-md">
+    <nav className="bg-white shadow-md fixed top-0 left-0 right-0 z-50">
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
